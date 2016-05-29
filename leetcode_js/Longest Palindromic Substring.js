75cfb45975845e3304eaff0a8bfeabb99412bed4
Qi_Lyu <huashiyiqike@qq.com>
2016-05-29 16:56:23 +0800

    Sun May 29 16:56:23 CST 2016

--- leetcode_js/Longest Palindromic Substring.js
+++ leetcode_js/Longest Palindromic Substring.js
@@ -3,29 +3,30 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-	var T = '^'+s.split("").join("#")+'$';
-	var n = T.length, C = R = 0, P = new Array(n), maxlen = 0, centerIndex = 0;
-	for(var i = 0; i < n; i++){
-		P[i] = 0;
-	}
-	for(var i = 1; i < n - 1; i++){
-		P[i] = R > i ? Math.min(R - i, P[2 * C - i]):0;
-		while(T[i + 1 + P[i]] == T[i - 1 - P[i]]){
-			P[i] += 1;
-		}
-		if(i + P[i] > R){
-			C = i;
-			R = i + P[i];
-		}
-	}
-	for(var i = 0 ; i < P.length; i++){
-		if(P[i] > maxlen){
-			maxlen = P[i];
-			centerIndex = i;
-		}
-	}
-	return s.slice(Math.floor((centerIndex - maxlen)/2),Math.floor((centerIndex + maxlen)/2));
+    var T = '^#'+s.split("").join("#")+'#$';
+    var n = T.length, C = R = 0, P = new Array(n), maxlen = 0, centerIndex = 0;
+    for(var i = 0; i < n; i++){
+        P[i] = 0;
+    }
+    for(var i = 1; i < n - 1; i++){
+        P[i] = R > i ? Math.min(R - i, P[2 * C - i]):0;
+        while(T[i + 1 + P[i]] == T[i - 1 - P[i]]){
+            P[i] += 1;
+        }
+        if(i + P[i] > R){
+            C = i;
+            R = i + P[i];
+        }
+    }
+    for(var i = 0 ; i < P.length; i++){
+        if(P[i] > maxlen){
+            maxlen = P[i];
+            centerIndex = i;
+        }
+    }
+    return s.slice(Math.floor((centerIndex - maxlen)/2),Math.floor((centerIndex + maxlen)/2));
 }
+
 
 
 //TLE
