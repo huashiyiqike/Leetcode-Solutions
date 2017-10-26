d0d0a5522672710e1118c7e6ea4352cf9d89c79e
Qi Lyu <lyuqi@baidu.com>
2017-10-26 21:54:13 +0800

    odd even linked list

--- leetcode_js/Odd Even Linked List.js
+++ leetcode_js/Odd Even Linked List.js
@@ -1,1 +1,32 @@
-  
+ /**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(!head) {
+        return head;
+    }
+    var even = new ListNode(null);
+    var current = head;
+    var currentEven = even;
+    while (!!current.next) {
+        currentEven.next = current.next;
+        currentEven = currentEven.next;
+        if (!!current.next.next) {
+            current.next = current.next.next;
+            current = current.next;
+            currentEven.next = current.next;
+        } else {
+            break;
+        }
+    }
+    current.next = even.next;
+    return head;
+}; 
--- leetcode_js/Palindrome Pairs.js
+++ leetcode_js/Palindrome Pairs.js
@@ -5,3 +5,39 @@
 var palindromePairs = function(words) {
     
 };
+
+/*
+https://discuss.leetcode.com/topic/46084/concise-python-solution
+class Solution(object):
+    def palindromePairs(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[int]]
+        """
+        res = []
+        dic = {}
+        for i in range(len(words)):
+            dic[words[i]] = i
+        for word in words:
+            word_rev = word[::-1]
+            if word_rev in dic and dic[word_rev] != dic[word]:
+                res.append([dic[word_rev], dic[word]])
+            for i in range(len(word)):
+                temp1 = word[:i][::-1]
+                temp2 = word_rev[:i]
+                if temp1 in dic and isPal(word[i:]):
+                    res.append([dic[word], dic[temp1]])
+                if temp2 in dic and isPal(word_rev[i:]):
+                    res.append([ dic[temp2], dic[word]])
+        return res
+def isPal(word):
+    left = 0
+    right = len(word)-1
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+    */
